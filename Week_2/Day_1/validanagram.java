dc3195f056d48ad6f06dae4077fb3ba34076e7dc
Nivedita Pandey <nivedita1530@gmail.com>
2026-06-08 23:43:26 +0530

    added valid anagram

---
+++ Week_2/Day_1/validanagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        int [] freq=new int[26];
+        for(int ele:s.toCharArray()){
+            freq[ele-'a']++;
+        }
+        for(int ele:t.toCharArray()){
+            freq[ele-'a']--;
+        }
+        for(int i:freq){
+            if(i!=0) return false;
+        }
+        return true;
+    }
+}
