7b4a2474b7961b78a1804e64a373856c7878db83
Nivedita Pandey <nivedita1530@gmail.com>
2026-06-10 23:52:37 +0530

    added is subsequence

---
+++ Week_2/Day_3/issubsequence.java
@@ -0,0 +1,17 @@
+package Week_2.Day_3;
+
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i=0;
+        int j=0;
+        while(i<s.length()&&j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        return i==s.length();
+    }
+} {
+    
+}
